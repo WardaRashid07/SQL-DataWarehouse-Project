e3419cbde443a9e2cd32e278cda123c91c6b2c77
wardarashid <wardahrashid07@gmail.com>
2025-09-29 20:38:02 +0500

    Add files via upload

---
+++ Advanced-Analytics/scripts/02_dimensions_inspection.sql
@@ -0,0 +1,28 @@
+
+/*
+===============================================================================
+Dimensions Exploration:
+Look at the data and identify dimensions and measures
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
